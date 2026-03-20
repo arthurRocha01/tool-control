238e914c24c72dc6f1746ecb7cb36cac6434c829
arthurRocha01 <arthurrocha007.net@gmail.com>
2026-03-20 17:01:37 -0300

    fix(server): corrige config vercel

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,6 @@ import app from './app.js'
 import { env } from './core/config/env.js'
 const PORT = env.server.port
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`)
+// })
