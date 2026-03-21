ab0aa4dc6d3c4939d29205ef0f1545ebfecd4479
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-03-21 12:25:49 -0300

    fix(client): login

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,32 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import { useAuthStore } from './store/authStore'
 import ProtectedRoute from './components/ProtectedRoute'
 import MainLayout from './layouts/MainLayout'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import Products from './pages/Products'
 import AddProduct from './pages/AddProduct'
 import ProductEdit from './pages/EditProduct'
 
 function App() {
-  const initAuth = useAuthStore((state) => state.initAuth)
+  ;<BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
 
-  useEffect(() => {
-    initAuth()
-  }, [initAuth])
+      <Route
+        path='/'
+        element={
+          <ProtectedRoute>
+            <MainLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path='add-product' element={<AddProduct />} />
+        <Route path='edit-product/:id' element={<ProductEdit />} />
+      </Route>
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-
-        <Route
-          path='/'
-          element={
-            <ProtectedRoute>
-              <MainLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Dashboard />} />
-          <Route path='products' element={<Products />} />
-          <Route path='add-product' element={<AddProduct />} />
-          <Route path='edit-product/:id' element={<ProductEdit />} />
-        </Route>
-
-        <Route path='*' element={<Navigate to='/' replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
