65a2c5841b219c943631bf7ce2aba6cf4f95091e
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-03-21 12:33:07 -0300

    fix(client): corrige verificação de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import ProtectedRoute from './components/ProtectedRoute'
+import { useAuthStore } from './store/authStore'
 import MainLayout from './layouts/MainLayout'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
@@ -7,25 +7,21 @@ import AddProduct from './pages/AddProduct'
 import ProductEdit from './pages/EditProduct'
 
 function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={!isAuthenticated ? <Login /> : <Navigate to='/' replace />} />
 
         <Route
           path='/'
-          element={
-            <ProtectedRoute>
-              <MainLayout />
-            </ProtectedRoute>
-          }
+          element={isAuthenticated ? <MainLayout /> : <Navigate to='/login' replace />}
         >
           <Route index element={<Dashboard />} />
           <Route path='add-product' element={<AddProduct />} />
           <Route path='edit-product/:id' element={<ProductEdit />} />
         </Route>
-
-        <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </BrowserRouter>
   )
