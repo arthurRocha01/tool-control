5fcc8ed75e728f883006031c00762cfcf528a666
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-03-20 23:30:38 -0300

    fix: corrije app.ts e vercel.json

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,10 +6,15 @@ import productRoutes from './modules/product/infra/http/product.routes.js'
 import authRoutes from './modules/auth/infra/http/auth.routes.js'
 
 const app: Application = express()
+
+const apiRouter = express.Router()
+
 app.use(express.json())
 app.use(cors())
 
 app.use('/produtos', productRoutes)
 app.use('/auth', authRoutes)
+
+app.use('/api', apiRouter)
 
 export default app
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
-import app from './app.js'
-import { env } from './core/config/env.js'
-const PORT = env.server.port
+// import app from './app.js'
+// import { env } from './core/config/env.js'
+// const PORT = env.server.port
 
 // app.listen(PORT, () => {
 //   console.log(`Servidor rodando na porta ${PORT}`)
