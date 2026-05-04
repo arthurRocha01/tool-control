03f9c7b11acbe1b4560cc36e5f0d3e6aeddb6dd1
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-05-04 10:59:55 -0300

    Corrige bug de roteamento do backend

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5050',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
